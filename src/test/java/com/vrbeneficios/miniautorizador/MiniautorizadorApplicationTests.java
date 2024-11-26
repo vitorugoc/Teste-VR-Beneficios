4b824ef9f7dced9973909a538b193028a1fff855
Vitor Cruz <vitor.cruz_iebt@123milhas.com.br>
2024-11-26 15:36:18 -0300

    raw: inicializando projeto

---
+++ src/test/java/com/vrbeneficios/miniautorizador/MiniautorizadorApplicationTests.java
@@ -0,0 +1,13 @@
+package com.vrbeneficios.miniautorizador;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MiniautorizadorApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
