ee10535a2e5c73e5cbede236b9ac6c73c7482f83
Vitor Cruz <vitor.cruz_iebt@123milhas.com.br>
2024-11-26 15:39:35 -0300

    feat: definindo model principal Cartao

---
+++ src/main/java/com/vrbeneficios/miniautorizador/model/Cartao.java
@@ -0,0 +1,14 @@
+package com.vrbeneficios.miniautorizador.model;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
+
+import java.math.BigDecimal;
+
+@Entity
+public class Cartao {
+    @Id
+    private String numeroCartao;
+    private String senha;
+    private BigDecimal saldo = BigDecimal.valueOf(500.00);
+}
