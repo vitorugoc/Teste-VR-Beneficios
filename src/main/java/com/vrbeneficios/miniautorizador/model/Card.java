e2635c89d47d42a9de9e3bdd0d51700f2010bfbd
Vitor Cruz <vitor.cruz_iebt@123milhas.com.br>
2024-11-26 15:57:17 -0300

    refactor: utilizando Lombok no model Card

--- src/main/java/com/vrbeneficios/miniautorizador/model/Card.java
+++ src/main/java/com/vrbeneficios/miniautorizador/model/Card.java
@@ -2,13 +2,24 @@ package com.vrbeneficios.miniautorizador.model;
 
 import jakarta.persistence.Entity;
 import jakarta.persistence.Id;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
 
 import java.math.BigDecimal;
 
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
 @Entity
 public class Card {
     @Id
-    private String numeroCartao;
-    private String senha;
-    private BigDecimal saldo = BigDecimal.valueOf(500.00);
+    private String cardNumber;
+    private String password;
+    private BigDecimal balance = BigDecimal.valueOf(500.00);
+
+    public Card(String cardNumber, String password) {
+        this.cardNumber = cardNumber;
+        this.password = password;
+    }
 }
