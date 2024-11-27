c980cb9ab510d67b37a8c9b2aba8bcd0eb0b6c3a
Vitor Cruz <vitor.cruz_iebt@123milhas.com.br>
2024-11-27 00:14:00 -0300

    refactor: extraindo as validacoes para metodos de validate

--- src/main/java/com/vrbeneficios/miniautorizador/service/TransactionService.java
+++ src/main/java/com/vrbeneficios/miniautorizador/service/TransactionService.java
@@ -7,6 +7,8 @@ import com.vrbeneficios.miniautorizador.repository.CardRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 
+import java.math.BigDecimal;
+
 @Service
 @RequiredArgsConstructor
 public class TransactionService {
@@ -17,15 +19,22 @@ public class TransactionService {
         Card card = cardRepository.findByCardNumber(transactionDTO.getCardNumber())
                 .orElseThrow(() -> new TransactionException("CARTAO_INEXISTENTE"));
 
-        if (!card.getPassword().equals(transactionDTO.getCardPassword())) {
-            throw new TransactionException("SENHA_INVALIDA");
-        }
-
-        if (card.getBalance().compareTo(transactionDTO.getValue()) < 0) {
-            throw new TransactionException("SALDO_INSUFICIENTE");
-        }
+        validatePassword(card, transactionDTO.getCardPassword());
+        validateBalance(card, transactionDTO.getValue());
 
         card.setBalance(card.getBalance().subtract(transactionDTO.getValue()));
         cardRepository.save(card);
+    }
+
+    private void validatePassword(Card card, String password) {
+        if (!card.getPassword().equals(password)) {
+            throw new TransactionException("SENHA_INVALIDA");
+        }
+    }
+
+    private void validateBalance(Card card, BigDecimal amount) {
+        if (card.getBalance().compareTo(amount) < 0) {
+            throw new TransactionException("SALDO_INSUFICIENTE");
+        }
     }
 }
