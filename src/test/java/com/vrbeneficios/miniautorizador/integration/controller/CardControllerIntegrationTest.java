3ecf78a8e5d0df9c3c3b1de1221ec81091de9b50
Vitor Cruz <vitor.cruz_iebt@123milhas.com.br>
2024-11-27 11:26:16 -0300

    test: criando testes de integracao para o CardController

---
+++ src/test/java/com/vrbeneficios/miniautorizador/integration/controller/CardControllerIntegrationTest.java
@@ -0,0 +1,91 @@
+package com.vrbeneficios.miniautorizador.integration.controller;
+
+import com.vrbeneficios.miniautorizador.dto.CardDTO;
+import com.vrbeneficios.miniautorizador.model.Card;
+import com.vrbeneficios.miniautorizador.repository.CardRepository;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.web.client.TestRestTemplate;
+import org.springframework.boot.test.web.server.LocalServerPort;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+
+import java.math.BigDecimal;
+import java.util.Objects;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
+class CardControllerIntegrationTest {
+
+    @LocalServerPort
+    private int port;
+
+    @Autowired
+    private TestRestTemplate restTemplate;
+
+    @Autowired
+    private CardRepository cardRepository;
+
+    @BeforeEach
+    void setupDatabase() {
+        cardRepository.deleteAll();
+        cardRepository.save(new Card("6549873025634501", "1234", BigDecimal.valueOf(500.00)));
+    }
+
+    @Test
+    void shouldCreateCardSuccessfully() {
+        CardDTO cardDTO = new CardDTO("1111222233334444", "5678");
+
+        ResponseEntity<CardDTO> response = restTemplate.postForEntity(
+                "http://localhost:" + port + "/cards",
+                cardDTO,
+                CardDTO.class
+        );
+
+        assertEquals(HttpStatus.CREATED, response.getStatusCode());
+        assertEquals(cardDTO.getCardNumber(), Objects.requireNonNull(response.getBody()).getCardNumber());
+        assertEquals(cardDTO.getPassword(), response.getBody().getPassword());
+    }
+
+    @Test
+    void shouldReturnUnprocessableEntityWhenCardAlreadyExists() {
+        CardDTO cardDTO = new CardDTO("6549873025634501", "1234");
+
+        ResponseEntity<CardDTO> response = restTemplate.postForEntity(
+                "http://localhost:" + port + "/cards",
+                cardDTO,
+                CardDTO.class
+        );
+
+        assertEquals(HttpStatus.UNPROCESSABLE_ENTITY, response.getStatusCode());
+        assertEquals(cardDTO.getCardNumber(), Objects.requireNonNull(response.getBody()).getCardNumber());
+    }
+
+    @Test
+    void shouldReturnBalanceForExistingCard() {
+        String cardNumber = "6549873025634501";
+
+        ResponseEntity<BigDecimal> response = restTemplate.getForEntity(
+                "http://localhost:" + port + "/cards/" + cardNumber,
+                BigDecimal.class
+        );
+
+        assertEquals(HttpStatus.OK, response.getStatusCode());
+        assertEquals(0, BigDecimal.valueOf(500.00).compareTo(response.getBody()), "The balances are not equal");
+    }
+
+    @Test
+    void shouldReturnNotFoundForNonExistentCard() {
+        String cardNumber = "0000111122223333";
+
+        ResponseEntity<String> response = restTemplate.getForEntity(
+                "http://localhost:" + port + "/cards/" + cardNumber,
+                String.class
+        );
+
+        assertEquals(HttpStatus.NOT_FOUND, response.getStatusCode());
+    }
+}
